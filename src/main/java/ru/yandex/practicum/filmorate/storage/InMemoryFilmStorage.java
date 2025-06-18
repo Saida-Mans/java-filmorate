94e8247896c0ab795fe2bf664d02a83b133ee887
Saida <saidasaifutdinova@yandex.ru>
2025-06-18 16:14:34 +0300

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Component
