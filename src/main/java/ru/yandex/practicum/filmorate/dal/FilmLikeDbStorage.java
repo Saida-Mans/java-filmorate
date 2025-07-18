b60b54af65184699b8431fe245db40736c9874d5
Saida <saidasaifutdinova@yandex.ru>
2025-07-19 00:02:20 +0300

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmLikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmLikeDbStorage.java
@@ -1,11 +1,8 @@
 package ru.yandex.practicum.filmorate.dal;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.util.HashSet;
 import java.util.Set;
